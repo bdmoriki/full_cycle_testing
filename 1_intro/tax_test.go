911ecd68b929995079c2a1a351da67660a2b5147
bdmoriki <dupim1@hotmail.com>
2024-07-16 21:06:46 -0300

    Adicionando aula de introducao de testes

---
+++ 1_intro/tax_test.go
@@ -0,0 +1,33 @@
+package tax
+
+import "testing"
+
+func TestCalculateTax(t *testing.T) {
+	amount := 500.0
+	expected := 5.0
+
+	result := CalculateTax(amount)
+
+	if result != expected {
+		t.Errorf("Expected %f but got %f ", expected, result)
+	}
+}
+
+func TestCalculateTaxBatch(t *testing.T) {
+	type calcTax struct {
+		amount, expect float64
+	}
+
+	table := []calcTax{
+		{500.00, 5.00},
+		{1000.00, 10.00},
+		{1500.00, 10.00},
+	}
+
+	for _, item := range table {
+		result := CalculateTax(item.amount)
+		if result != item.expect {
+			t.Errorf("Expected %f but got %f ", item.expect, result)
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bdmoriki/full_cycle_testing
+
+go 1.22.1
