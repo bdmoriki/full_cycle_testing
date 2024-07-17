7848180ecf4fed1964895c26659b996be3f33483
bdmoriki <dupim1@hotmail.com>
2024-07-16 22:30:52 -0300

    Adicionando aula de fuzzing

--- 1_intro/tax_test.go
+++ 1_intro/tax_test.go
@@ -35,6 +35,22 @@ func TestCalculateTaxBatch(t *testing.T) {
 
 func BenchmarkCalculateTax(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		CalculateTax2(500.0)
+		CalculateTax(500.0)
 	}
+}
+
+func FuzzCalculateTax(f *testing.F) {
+	seed := []float64{-1, -2, 2.5, 500.0, 1000.0, 1501.01}
+	for _, amount := range seed {
+		f.Add(amount)
+	}
+	f.Fuzz(func(t *testing.T, amount float64) {
+		result := CalculateTax(amount)
+		if amount <= 0 && result != 0 {
+			t.Errorf("Received %f but expected 0", result)
+		}
+		if amount >= 20000 && result != 20 {
+			t.Errorf("Received %f but expected 20", result)
+		}
+	})
 }
--- Readme.md
+++ Readme.md
@@ -6,3 +6,6 @@ go test -bench=. -run=^#
 go test -bench=. -run=^# -count=10
 go test -bench=. -run=^# -count=100 -benchtime=3s
 go test -bench=. -run=^# -benchmem
+
+go test -fuzz=. 
+go test -fuzz=. -run=ˆ= -fuzztime=5s
